2e6292b6c7460c8ca4fe61a0acbdea713aa45864
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 04:49:37 +0300

    :wrench: Instance method, calling our instance method

--- lib/book.rb
+++ lib/book.rb
@@ -3,15 +3,42 @@ class Book
     attr_reader :title ,:author ,:page_count ,:genre
     
     #defining initalizatin method that takes an arguement title
-    def initialize(title,author,page_count,genre)
+    def initialize(title)
         @title = title
+    end
+
+    #instance author method 
+    def author(author)
         @author = author
+       
+    end
+
+     #instance method 
+     def page_count(page_count)
         @page_count = page_count
-        @genre = genre
         
     end
+
+     #instance method 
+     def genre(genre)
+        @genre = genre
+    end
+
 end
 #creating an instance of our new class 
-books = Book.new
+books = Book.new("And Then There Were None")
+#puts the title 
+puts books.title
+
+#Puts properties of book here
+
+puts books.author("Agatha Christie")
+
+puts books.page_count(272)
+
+puts books.genre("Mystery")
+
+
+
 
 

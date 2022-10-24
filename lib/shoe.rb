a9298e5aacf7132c056a459551d28a526c6a348a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 21:46:08 +0300

    Done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,29 @@
-#defining macro variable attr_reader
-attr_accessor :brand, :color, :size, :material, :condition
+
 class Shoe
+
+    #defining macro variable attr_reader
+    attr_accessor :brand, :color, :size, :material, :condition
+
     #defining initialization method that takes an arguement title
-    def initialize(brand)
+    def initialize (brand)
         @brand = brand
     end
 
     #defining our cobble method
     def cobble
-        puts "shoe has been repaired"  
-    end
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+        end
+         
 end
 #creating an instance of our new class 
-shoes = Shoe.new("Adidas")
+shoe = Shoe.new("Nike")
+
+#puts the color 
+puts shoe.color = "red"
+
+#puts the shoe size
+ puts shoe.size = 9.5
+
+ #puts the shoe genre
+  puts shoe.material = "suede"

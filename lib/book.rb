22cd0431e92c52c475d0c745e49eab7b13797756
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 05:31:31 +0300

    :recycle: Code refactored

--- lib/book.rb
+++ lib/book.rb
@@ -1,42 +1,26 @@
 class Book
     #defining macro variable attr_reader 
-    attr_reader :title ,:author ,:page_count ,:genre
+    attr_accessor  :title ,:author ,:page_count  ,:genre 
     
     #defining initalizatin method that takes an arguement title
     def initialize(title)
         @title = title
     end
 
-    #instance author method 
-    def author(author)
-        @author = author
-       
-    end
-
-     #instance method 
-     def page_count(page_count)
-        @page_count = page_count
-        
-    end
-
-     #instance method 
-     def genre(genre)
-        @genre = genre
-    end
-
+    #defining our turn method 
+    def turn_page
+        puts "Flipping the page ... wow, you read fast!"
+    end 
 end
 #creating an instance of our new class 
 books = Book.new("And Then There Were None")
-#puts the title 
-puts books.title
 
-#Puts properties of book here
-
-puts books.author("Agatha Christie")
-
-puts books.page_count(272)
-
-puts books.genre("Mystery")
+#puts the author 
+puts books.author = "Agatha Christie"
+#puts the page count 
+puts books.page_count = 272
+#puts the genre
+puts books.genre = "Mystery"
 
 
 

6787dcec8eb2f2f3c429148b7fcf6fe91f56f5ea
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 20:53:34 +0300

    :wrench:defining macro variable attr_reader

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
-# Make your shoe class here!
+#defining macro variable attr_reader
+attr_accessor :brand, :color, :size, :material, :condition
 class Shoe
     #defining initialization method that takes an arguement title
     def initialize(brand)
         @brand = brand
     end
-    
+
         
     end
 end

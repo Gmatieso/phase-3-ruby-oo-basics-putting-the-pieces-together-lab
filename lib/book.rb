51d957dab591a45d80ecae59480ea680f1621288
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 04:17:01 +0300

    :recycle:

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,13 @@
 class Book
-    def initialize(title)
+    #defining macro variable attr_reader 
+    attr_reader :title ,:author ,:page_count ,:genre
+    
+    #defining initalizatin method that takes an arguement title
+    def initialize(title,author,page_count,genre)
         @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
         
     end
 end
